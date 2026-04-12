3e7b0c9bada095c36d367f64303a205f7f9ff8cd
Martu410 <marti_djelepov@tutanota.com>
2026-04-12 22:45:37 +0300

    Имплементиране на първична логика с рекурсивен DFS и добавяне на AutomatonManager

--- src/automata/core/Automaton.java
+++ src/automata/core/Automaton.java
@@ -1,12 +1,14 @@
-package automata;
+package automata.core;
 
 import automata.model.State;
 import automata.model.Transition;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-public class Automaton {
+public class Automaton implements Recognizable {
     private String id;
     private State startState;
     private List<State> states;
@@ -15,29 +17,89 @@ public class Automaton {
 
     public Automaton(String id) {
         this.id = id;
-        this.states=new ArrayList<>();
-        this.transitions=new ArrayList<>();
+        this.states = new ArrayList<>();
+        this.transitions = new ArrayList<>();
     }
+
     public String getId() {
         return id;
     }
+
     public void setId(String id) {
         this.id = id;
     }
+
     public State getStartState() {
         return startState;
     }
+
     public void setStartState(State startState) {
         this.startState = startState;
     }
+
     public List<State> getStates() {
         return states;
     }
+
     public List<Transition> getTransitions() {
         return transitions;
     }
-    public void addTransition(State from, char symbol, State to){
+
+    public void addState(State state) {
+        states.add(state);
+    }
+
+    public void addTransition(State from, char symbol, State to) {
         Transition newTransition = new Transition(from, symbol, to);
         this.transitions.add(newTransition);
+    }
+
+
+    public Set<State> getEpsilonClosure(Set<State> initialStates) {
+        Set<State> closure = new HashSet<>(initialStates);
+        for (State s : initialStates) {
+            epsilonDFS(s, closure);
+        }
+        return closure;
+    }
+
+    private void epsilonDFS(State current, Set<State> closure) {
+        for (Transition t : transitions) {
+            if (t.getFrom().equals(current) && t.getSymbol() == 'E') {
+                if (closure.add(t.getTo())) {
+                    epsilonDFS(t.getTo(), closure);
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean recognize(String word) {
+        if (startState == null) return false;
+        Set<State> current = new HashSet<>();
+        current.add(startState);
+        current = getEpsilonClosure(current);
+
+        for (char symbol : word.toCharArray()) {
+            Set<State> next = new HashSet<>();
+            for (State state : current) {
+                // Бавно сканиране на списъка
+                for (Transition t : transitions) {
+                    if (t.getFrom().equals(state) && t.getSymbol() == symbol) {
+                        next.add(t.getTo());
+                    }
+                }
+            }
+            current = getEpsilonClosure(next);
+        }
+        for (State state : current) {
+            if (state.isAccepting()) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return !recognize("");
     }
 }
--- src/automata/manager/AutomatonManager.java
+++ src/automata/manager/AutomatonManager.java
@@ -1,4 +1,40 @@
 package automata.manager;
 
+import automata.core.Automaton;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class AutomatonManager {
+    private Map<String, Automaton> automataMap;
+
+    public AutomatonManager() {
+        this.automataMap = new HashMap<>();
+    }
+
+    public void addAutomaton(Automaton a) {
+        automataMap.put(a.getId(), a);
+    }
+
+    public Automaton getAutomaton(String id) {
+        return automataMap.get(id);
+    }
+
+    public void clear() {
+        automataMap.clear();
+    }
+
+    public void listAutomata() {
+        if (automataMap.isEmpty()) {
+            System.out.println("Няма заредени автомати в паметта.");
+            return;
+        }
+        System.out.println("Налични автомати:");
+        for (String id : automataMap.keySet()) System.out.println("  - " + id);
+    }
+
+    public Map<String, Automaton> getAllAutomata() {
+        return Collections.unmodifiableMap(automataMap);
+    }
 }
